0cf82cf432d991a75c31136f8350a0c933f8df6d
rvm9902 <146854064+rvm9902@users.noreply.github.com>
2025-03-21 10:28:20 -0300

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,27 @@
+package br.com.petshop;
+
+
+public class Animal {
+    private String nome;
+    private int idade;
+
+   
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+   
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome + ", Idade: " + idade + " anos");
+    }
+}
